7bb3006aa8f1ab4331e426312dcb2b692efdd384
Regynald Zambrano <regynald.zambrano@gmail.com>
2024-10-23 11:07:56 -0500

    set new remote

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -4,7 +4,7 @@ import { View, StyleSheet } from 'react-native';
 export default function NotFoundScreen() {
     return (
         <>
-            <Stack.Screen options={{ title: "Oops! This screen doesn't exist." }} />
+            <Stack.Screen options={{ title: "Oops! This page doesn't exist." }} />
             <View style={styles.container}>
                 <Link href='/'>Go to home screen</Link>
             </View>
